e54fe83ced4cf3ee5a6fc324f3d033c96ed45be2
Anameskhi <a.meskhii@gmail.com>
2023-06-20 16:44:09 +0400

    fix bugs

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,14 +1,14 @@
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { CommonModule, NgFor } from '@angular/common';
-import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
 import { ITodo } from 'src/app/common/interfaces/todo.interface';
 import { TodoService } from 'src/app/common/services/todo.service';
 import {  MatIconModule } from '@angular/material/icon';
-import { PersonService } from 'src/app/common/services/person.service';
-import { IPerson } from 'src/app/common/interfaces/person.interface';
 import { StorageService } from 'src/app/common/services/storage.service';
 import { TodoStatus } from 'src/app/common/types/todo-status';
+
+
 
 @Component({
   selector: 'app-list',
@@ -17,20 +17,27 @@ import { TodoStatus } from 'src/app/common/types/todo-status';
   standalone: true,
   imports: [CdkDropList, NgFor, CdkDrag,MatIconModule,CommonModule,RouterModule],
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit {
   todo: ITodo[] = [];
   inProgress: ITodo[] = [];
   done: ITodo[] = [];
 
-  persons: IPerson[] = []
-   
-   todos = this.todoService.todosSub.subscribe(res=>{this.todo.push(res)})
+  constructor(
+    private todoService: TodoService,
+    private storageService: StorageService,
+    private router: Router
+  ) {}
 
-  fetchListsFromStorage() {
+  ngOnInit(): void {
+    this.getTodos();
+  }
+  todos = this.todoService.todosSub.subscribe(res => { this.todo = [res, ...this.todo] })
+
+  getTodos() {
     this.todoService.getTodos().subscribe((todos: ITodo[]) => {
-      this.todo = todos.filter(todo => todo.status === 'todo');
-      this.inProgress = todos.filter(todo => todo.status === 'pending');
-      this.done = todos.filter(todo => todo.status === 'completed');
+      this.todo = todos.filter((todo) => todo.status === 'todo');
+      this.inProgress = todos.filter((todo) => todo.status === 'pending');
+      this.done = todos.filter((todo) => todo.status === 'completed');
     });
   }
 
@@ -47,21 +54,28 @@ export class ListComponent implements OnInit{
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
-
-      const updatedTodos = [...this.todo, ...this.inProgress, ...this.done];
-      const todoToUpdate = updatedTodos[event.currentIndex];
-      
-      const status = this.getListStatus(event.container.id);
-      console.log(event.container.id)
-      todoToUpdate.status = status;
-  
-      this.saveListsToStorage();
-      this.todoService.updateTodoById(todoToUpdate.id, todoToUpdate).subscribe(() => {
-      });
     }
+
+    // Update the status of the items in the source container
+    this.updateItemStatus(event.previousContainer.data, event.previousContainer.id);
+
+    // Update the status of the items in the target container
+    this.updateItemStatus(event.container.data, event.container.id);
+
+    // Save the updated lists to storage
+    this.saveListsToStorage();
   }
+
+  updateItemStatus(items: ITodo[], containerId: string) {
+    const status = this.getListStatus(containerId);
+
+    items.forEach((item) => {
+      item.status = status;
+    });
+  }
+
   getListStatus(containerId: string): TodoStatus {
     switch (containerId) {
       case 'cdk-drop-list-0':
@@ -74,28 +88,10 @@ export class ListComponent implements OnInit{
         return 'todo';
     }
   }
-  constructor(
-    private todoService: TodoService,
-    private router: Router,
-    private storageService: StorageService
-  ){}
-
-  ngOnInit(): void {
-   this.getTodo()
-   this.fetchListsFromStorage()
-  //  console.log(this.todo,this.inProgress,this.done)
-  }
-
-  getTodo(){
-    this.todoService.getTodos().subscribe(res => {
-      this.todo = res;
-      this.fetchListsFromStorage(); 
-    });
-  }
 
   delete(id: string) {
     this.todoService.deleteTodoById(id).subscribe(() => {
-      this.getTodo()
+      this.getTodos()
     });
   }
 
